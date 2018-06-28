c781ebe66005303f9ceebc6017989ce8751be7f0
Lucas Lusa <lucas.lusa_BOLS@compasso.com.br>
2018-06-28 10:24:35 -0300

    Método post concluido

--- index.js
+++ index.js
@@ -72,12 +72,15 @@ app.get('/api/profile/:userId', function (req, res) {
     let idUser = req.params.userId
     let encontrou = false;
     var arrFound = data.filter(function(item) {
-        if(item.uuid == idUser)
+        if(item != null)
         {
-            let usr = {item}
-            res.status(200).json(usr)
-            encontrou = true;
-            return
+            if(item.uuid == idUser)
+            {
+                let usr = {item}
+                res.status(200).json(usr)
+                encontrou = true;
+                return
+            }
         }
       });
       if(encontrou == false)
@@ -90,25 +93,34 @@ app.get('/api/profile/:userId', function (req, res) {
 app.delete('/api/profile', function (req, res) {
     const data = require("./model/profile.json")
     
-    console.log("Iniciado /api/profile");
-    console.log("remoção de usuario");
-    
-
     let user = req.headers.uuid
     let temp = 0
+    let encontrou = false;    
     var arrFound = data.filter(function(item) {
-        if(item.uuid == user)
+        if(item != null)
         {
-            let usr = {item}
-            console.log(item)
-            delete data[temp]
-            return
+            if(item.uuid == user)
+            {
+                let usr = {item}
+                delete data[temp]
+                encontrou = true;  
+                res.status(200).send("Sucesso!!!")                   
+                return
+            }
         }
         temp = temp + 1
-        
       });
-      res.status(200).send(data)       
       
+      fs.writeFile("./model/profile.json", JSON.stringify(data, false, 2), function(err) {
+        if(err) {
+            return console.log(err);
+        }
+        console.log("The file was saved!");
+    }); 
+    if(encontrou == false)
+      {
+        res.status(401).send("ID de usuário não encontrado")       
+      }
     console.log("recebido usuario: " + user)
     
     
@@ -116,6 +128,84 @@ app.delete('/api/profile', function (req, res) {
 )
     
 
+app.post('/api/profile/', verifyToken, function (req, res) {
+    
+        let auth = req.headers.authorization
+        if (auth) {
+            const data = require("./model/profile.json")
+            let idUser = req.params.userId
+            let encontrou = false;
+            let temp = 0
+            
+            let newuser = {
+                "uuid": req.headers.uuid,
+                "gender": req.headers.gender,
+                "title": req.headers.title,
+                "first": req.headers.first,
+                "last": req.headers.last,
+                "email": req.headers.emailuuid,
+                "phone": req.headers.phone,
+                "cep": req.headers.cep,
+                "thumbnail": req.headers.thumbnail
+            };
+            var arrFound = data.filter(function (item) {
+                if(item != null)
+                {
+                    if (item.uuid == newuser.uuid) {
+                        data[temp] = newuser
+                        encontrou=true
+                        return
+                    }
+                }
+                temp = temp + 1
+            });
+            
+            if (encontrou == false) {
+                res.status(401).send("ID de usuário não encontrado")
+            }
+            else
+                res.status(200).send("Alterado com sucesso!")
+            
+            fs.writeFile("./model/profile.json", JSON.stringify(data, false, 2), function (err) {
+                if (err) {
+                    return console.log(err);
+                }
+                console.log("The file was saved!");
+            });
+            
+        }
+        else {
+            res.status(401).send("Acesso negado");
+        }
+    
+    
+    }
+    )
+
 app.listen(port, function () {
     console.log("Rodando na porta:", port)
 })
+
+
+
+
+
+
+
+
+
+//++++++++++++++++++++++++++
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++
+/**
+ 
+location (consulta de endereço via cep)
+localhost:4000/address/67419
+
+{
+    "cep": 67419,
+    "rua": "rua piauí",
+    "cidade": "macapá",
+    "estado": "rio de janeiro"
+}
+ */
